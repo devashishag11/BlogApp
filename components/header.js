d3c7ea4dad137ca55920d139092501a12bb7cbdc
Devashish Agarwal <73063420+devashishag11@users.noreply.github.com>
2023-08-06 15:29:58 +0530

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,9 +1,9 @@
 const header = () => {
   return (
     <header className ="bg-gray-300">
-        <div className="xl:container xl:mx-auto flex flex-col items sm:flex-row sm:justify-between text-center py-3 text-red-500">
+        <div className="xl:container xl:mx-auto flex flex-col items sm:flex-row sm:justify-between text-center py-3 text-black">
           <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
-            <input type="text" placeholder="Search..."/>
+            <input type="text" className= "input-text" placeholder="Search..."/>
           </div>
           <div className="shrink w-80 sm:order-2">
             <a>Design</a>
